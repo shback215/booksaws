38584fe8d5c410bd739bdc06a9f11e7e71b4a12d
shback215 <shbackup2252@gmail.com>
2026-04-26 22:28:24 -0400

    Initial commit - reactaws  website

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About'
+
+function App() {
+  return (
+    <div className="app">
+      <nav>
+        <Link to="/">Home</Link>
+        <span> </span>
+        <Link to="/about">About</Link>
+      </nav>
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booksreact" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
