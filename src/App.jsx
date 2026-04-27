0707f9bc6a3b9804e41d95a2a7cdd18b5a2f0e67
shback215 <shbackup2252@gmail.com>
2026-04-27 00:46:37 -0400

    2nd Update for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
