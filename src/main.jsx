7c754e1ca8ba46c03c2bc4344ad4be9335117ba7
shback215 <shbackup2252@gmail.com>
2026-05-18 13:14:19 -0400

    Changes for Netlify

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/booksaws">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
